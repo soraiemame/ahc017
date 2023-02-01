51fd9d3f975bb49ad06132c9d4d07949ec2f1e80
soraiemame <funasou46@yahoo.co.jp>
2023-02-01 17:34:49 +0900

    testheavy job

--- run.py
+++ run.py
@@ -1,9 +1,10 @@
 from subprocess import *
 from time import sleep
+from sys import stderr
 
 run("cargo build --release",shell=True)
 
 for i in range(10):
-    print("Hello!!")
+    print("Hello!!",file=stderr)
     sleep(1)
 
